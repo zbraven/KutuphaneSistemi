0d42d0b19aa842c37959f6871e934f56e19ea87e
Zahid Buğra Sarıcan <115879495+zbraven@users.noreply.github.com>
2023-12-09 00:06:00 +0300

    DB'ye bağlantım sorunsuz şekilde gerçekleşti. Tablomu oluşturdum. Controller'a bağlı olarak ilk defa View de ekledim.

---
+++ ZeroToHeroZBS/Controllers/KitapTuruController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ZeroToHeroZBS.Controllers
+{
+    public class KitapTuruController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
