edc717afa8e7300cf95ca6ad313ce9297bb2a2b6
Zahid Buğra Sarıcan <115879495+zbraven@users.noreply.github.com>
2023-12-08 21:20:45 +0300

    Add project files.

---
+++ ZeroToHeroZBS/Models/KitapTuru.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ZeroToHeroZBS.Models
+{
+    public class KitapTuru
+    {
+        [Key]
+        public int Id { get; set; }
+
+
+        [Required]
+        public string Ad { get; set; }
+    }
+}
---
+++ ZeroToHeroZBS/Utility/UygulamaDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Hosting;
+using ZeroToHeroZBS.Models;
+
+namespace ZeroToHeroZBS.Utility
+{
+    public class UygulamaDbContext:DbContext
+    {
+        public UygulamaDbContext(DbContextOptions<UygulamaDbContext> options):base(options)
+        {
+        }
+
+        public DbSet<KitapTuru> KitapTurleri => Set<KitapTuru>();
+    }
+}
