5dcfd3e6f1ceb010552ffad413f4ff9bbc23796f
Zahid Buğra Sarıcan <115879495+zbraven@users.noreply.github.com>
2023-12-09 01:13:46 +0300

    Context'i nasıl çağırdık?Program.cs sayesinde.  Türetme işlemini new'leyerek yapmamıza gerek kalmadı çünkü artık Singleton Pattern'e ait olan Dependency Injection kullandık.

--- ZeroToHeroZBS/Controllers/KitapTuruController.cs
+++ ZeroToHeroZBS/Controllers/KitapTuruController.cs
@@ -1,11 +1,21 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using ZeroToHeroZBS.Models;
+using ZeroToHeroZBS.Utility;
 
 namespace ZeroToHeroZBS.Controllers
 {
     public class KitapTuruController : Controller
     {
+        private readonly UygulamaDbContext _uygulamaDbContext;
+        public KitapTuruController(UygulamaDbContext context)
+        {
+            _uygulamaDbContext = context;
+        }
+           
         public IActionResult Index()
         {
+            List<KitapTuru> objKitapTuruList = _uygulamaDbContext.KitapTurleri.ToList();
+
             return View();
         }
     }
