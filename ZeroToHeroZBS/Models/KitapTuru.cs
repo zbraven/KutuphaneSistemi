d80618ee9c432c496776acaeeb34aa122e6d7ca5
Zahid Buğra Sarıcan <115879495+zbraven@users.noreply.github.com>
2023-12-11 15:04:13 +0300

    View'da asp-for kullanımı ve amacını gördüm. Veritabanındaki adıyla, görünmesi gereken adın ayarlamasını yaptım class içerisinde.

--- ZeroToHeroZBS/Models/KitapTuru.cs
+++ ZeroToHeroZBS/Models/KitapTuru.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace ZeroToHeroZBS.Models
 {
@@ -9,6 +10,7 @@ namespace ZeroToHeroZBS.Models
 
 
         [Required]
+        [DisplayName("Kitap Türü Adı")]
         public string Ad { get; set; }
     }
 }
